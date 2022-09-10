953cf9408fecc29da855144a651f3f48ef3aec74
Victor Ebegbuna <victor4real2000ng@hotmail.com>
2022-09-10 15:12:27 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - lowercase alphabets
+ *
+ * Return: Always (win)
+ * */
+ int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+
+	putchar('\n');
+	return (0)
+}
