ef2a09d4d16c4c8a9705ace00e944c70bbca1fda
Victor Ebegbuna <victor4real2000ng@hotmail.com>
2022-09-14 19:10:45 -0700

    task2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,16 +6,14 @@
  */
 void print_alphabet_x10(void)
 {
-		char c, i;
+	char c, i;
 
-		for (i = 0; i <= 9; i++)
+	for (i = 0; i <= 9; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
 		{
-			for (c = 'a'; c <= 'z'; c++)
-	
-				_putchar(c);
-			}
-			_putchar('\n');
+			_putchar(c);
 		}
+		_putchar('\n');
+	}
 }
-
-
