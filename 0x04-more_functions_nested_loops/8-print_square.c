26fd3c7cc25334bd9bdfda96844cfbe7c2d762c8
Victor Ebegbuna <victor4real2000ng@hotmail.com>
2022-09-15 18:09:06 -0700

    task8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -7,15 +7,6 @@
 *Return: returns nothing
 */
 
-#include <stdio.h>
-#include "main.h"
-
-/**
- * *print_square - prints squares
- * *@size: parameter
- * *Return: returns nothing
- * */
-
 void print_square(int size)
 {
 	int inc1, inc2;
