6feaa6beb93bf685b555d91df694528b279b9dc4
Victor Ebegbuna <victor4real2000ng@hotmail.com>
2022-09-21 16:36:42 -0700

    task11

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -5,8 +5,8 @@
  * @n1: first number
  * @n2: second number
  * @r: buffer for result
- * * @size_r: buffer size
- * *
+ * @size_r: buffer size
+ *
  * Return: address of r or 0
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
