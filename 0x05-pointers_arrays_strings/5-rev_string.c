42270b761a88a4eccd4a8628d7121fef12f4a924
Victor Ebegbuna <victor4real2000ng@hotmail.com>
2022-09-20 20:41:25 -0700

    task5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
@@ -13,9 +12,9 @@ void rev_string (char *s)
 	int l, i;
 	char ch;
 
-	for (l = 0; s[l] ! = '\0'; l++)
+	for(l = 0; s[l] ! = '\0'; l++)
 
-	for (i = 0; i < l / 2; i++)
+	for(i = 0; i < l / 2; i++)
 	{
 		ch = s[l];
 		s[l] = s[l - 1 - i];
