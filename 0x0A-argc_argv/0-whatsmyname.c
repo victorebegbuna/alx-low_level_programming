c8f178dd1ed393e282d7a75c2552801850ee4f6b
Victor Ebegbuna <victor4real2000ng@hotmail.com>
2022-09-30 21:42:48 -0700

    task1

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
-#include "main.h"
 
 /**
  * main - Print the name of the program
  * @argic: Count arguments
  * @argv: Arguments
+ * You should not removethe path before the name of the program 
  *
  * REturn: Always 0 (Success)
  */
 
 int main(int argc, char *argv[])
 {
-(void) argc;
-printf("%s\n", argc[0];
+	(void) argc;
+	printf("%s\n", argc[0]);
 
 	return (0);
 }
