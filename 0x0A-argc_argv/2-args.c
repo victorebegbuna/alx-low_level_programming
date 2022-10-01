6873b9fb56661e7cefb6c24df12519a05e6abdd9
Victor Ebegbuna <victor4real2000ng@hotmail.com>
2022-09-30 22:13:30 -0700

    task2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 
 /**
- * main - prints all the arguments a program receives.
+ *main - prints all the arguments a program receives.
  *
- * @argc: number of arguments
+ *@argc: number of arguments
  *
- * @argv: array of arguments.
+ *@argv: array of arguments.
  *
- * Return: Always (0) success.
+ *Return: Always (0) success.
  */
 
 int main(int argc, char *argv[])
