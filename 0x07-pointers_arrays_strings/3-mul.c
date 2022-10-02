625b8114fe364e819a53c2c16241cdb0d68d8021
Victor Ebegbuna <victor4real2000ng@hotmail.com>
2022-10-01 18:34:00 -0700

    task3

--- 0x07-pointers_arrays_strings/3-mul.c
+++ 0x07-pointers_arrays_strings/3-mul.c
@@ -4,20 +4,23 @@
  * main - program that prints its name, followed by a new line
  * @argc: argument that counts argument input
  * @argv: argument that stores the strings in an array of char* (strings)
- * Return: 0
+ *
+ * Return: 0;
  */
 int main(int argc, char *argv[])
 {
-	int num_1, num_2, mul;
+	int index, multiplication;
 
-	if (argc != 3)
-		printf("Error\n");
-	else
+	multiplication = 1;
+	if (argc < 3)
 	{
-		num_1 = atoi(argv[1]);
-		num_2 = atoi(argv[2]);
-		mul = num_1 * num_2;
-		printf("%d\n", mul);
+		prntf("Error\n");
+		return (1);
 	}
+	for (index = 1; index < argc; index++)
+	{
+		multiplication = multiplication * atoi(argv[index]);
+	}
+	printf("%d\n", multiplication);
 	return (0);
 }
