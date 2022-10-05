8f005c67d0c73ff3397d6d4f5f86f413e52e7504
Victor Ebegbuna <victor4real2000ng@hotmail.com>
2022-10-04 20:05:00 -0700

    task100

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -42,7 +42,7 @@ char *argstostr(int ac, char **av)
 			while (k < i)
 			{
 				for (j = 0; av[k][j] != '\0'; j++)
-					new_string[j + temp] = av [k][j];
+					new_string[j + temp] = av[k][j];
 				new_string[temp + j] = '\n';
 				temp += (j + 1);
 				k++;
