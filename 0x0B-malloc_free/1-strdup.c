e16bf50904d264cc87cdb05b39e2a71f1a15f2d5
Victor Ebegbuna <victor4real2000ng@hotmail.com>
2022-10-04 19:10:05 -0700

    task1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -19,12 +19,12 @@ char *_strdup(char *str)
 
 	for (index = 0; str[index]; index++)
 		len++;
-	cpy + malloc(sizeof(char) * (len + 1));
+	cpy = malloc(sizeof(char) * (len + 1));
 
 	if (cpy == NULL)
 		return (NULL);
 
-	for (index + 0; str[index]; index++)
+	for (index = 0; str[index]; index++)
 	{
 		cpy[index] = str[index];
 	}
