50dbabebf3c4bf0584c7ed952b71d281afec3eab
Victor Ebegbuna <victor4real2000ng@hotmail.com>
2022-10-05 20:32:22 -0700

    task2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -27,7 +27,7 @@ char *str_concat(char *s1, char *s2)
 	if (concat_str == NULL)
 		return (NULL);
 
-	for (index = 0, s1[index]; index++)
+	for (index = 0; s1[index]; index++)
 		concat_str[concat_index++] = s1[index];
 
 	for (index = 0; s2[index]; index++)
