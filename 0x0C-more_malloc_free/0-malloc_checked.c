51bd23eb68681d4f2c4a4e38d8603cbccb1ba188
Victor Ebegbuna <victor4real2000ng@hotmail.com>
2022-10-06 21:14:28 -0700

    task0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -9,12 +9,12 @@
  */
 void *malloc_checked(unsigned int b)
 {
-	    void *mem = malloc(b);
+	    void *p;
 
-	        if (mem == NULL)
+	    p = malloc(b)
+	    if (mem == NULL)
 		        exit(98);
-
-		    return (mem);
+	    return (p);
 }
 
 
