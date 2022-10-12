cd6455f2b6e7f0d032dfe5042c3225b2f8ec762d
Victor Ebegbuna <victor4real2000ng@hotmail.com>
2022-10-12 15:03:08 -0700

    task0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -6,8 +6,7 @@
  * @name: name to print
  * @f: pointer to the printing function
  */
-
-void print_name(char *name, void (*f)(char))
+void print_name(char *name, void (*f)(char *))
 {
 	if (!name || !f)
 		return;
